1b168a2be75fe2e32464bd8694f236a0e7788dcc
Andrey Bykanov <adm@mymsk.cloud>
2020-02-25 22:39:55 +0300

    fixed hostname detection

--- deploy/run.sh
+++ deploy/run.sh
@@ -4,8 +4,10 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 
 cd /etc/borg/deploy
 
-SERVER_NAME=`hostname`
+SERVER_NAME=`hostname -A`
 RSA=`ssh-keyscan -t rsa $SERVER_NAME | awk '{print $3}'`
+
+mkdir -p roles/add_borg_system/vars/
 
 echo "---"                            > roles/add_borg_system/vars/main.yml
 echo "borg_server_name: $SERVER_NAME">> roles/add_borg_system/vars/main.yml
