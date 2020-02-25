6a1e9af333502c9d59ac3f4f0460028bf066e246
root <root@backup>
2020-02-25 22:34:28 +0300

    Add variable generation

--- deploy/run.sh
+++ deploy/run.sh
@@ -4,5 +4,13 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 
 cd /etc/borg/deploy
 
+SERVER_NAME=`hostname`
+RSA=`ssh-keyscan -t rsa $SERVER_NAME | awk '{print $3}'`
+
+echo "---"                            > roles/add_borg_system/vars/main.yml
+echo "borg_server_name: $SERVER_NAME">> roles/add_borg_system/vars/main.yml
+echo "borg_server_pubrsakey: $RSA"   >> roles/add_borg_system/vars/main.yml
+
+
 ansible-playbook playbooks/backup.yml -i inventory
 
